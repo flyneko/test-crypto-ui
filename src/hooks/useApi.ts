3ff6ab126b1c3ce42f9603a0c51e02b63a814f5f
Evgeniy <shiroleer@gmail.com>
2022-04-02 17:25:52 -0700

    useApi hook: add callOnInit option

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,25 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default (kyRequest: any) => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const request = async (args?: any) => {
-    setIsLoading(true);
-    return kyRequest(args)().then((response: any) => {
-      setData(response);
-      return response;
-    }).catch((e: any) => {
-      setError(e.message || "Unexpected Error!");
-    }).finally(() => {
-      setIsLoading(false);
-    })
-  };
+interface RequestOptions {
+    callOnInit?: boolean
+    callInitData?: any
+}
 
-  return {
-    data,
-    error,
-    isLoading,
-    request
-  };
+export default (kyRequest: any, options: RequestOptions = {}) => {
+    const [data, setData] = useState({} as any);
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const request = async (args?: any) => {
+        setIsLoading(true);
+        return kyRequest(args)().then((response: any) => {
+            setData(response);
+            return response;
+        }).catch((e: any) => {
+            setError(e.message || "Unexpected Error!");
+        }).finally(() => {
+            setIsLoading(false);
+        })
+    };
+
+    useEffect(() => {
+        options.callOnInit && request(options.callInitData || {});
+    }, [])
+
+    return {
+        data,
+        error,
+        isLoading,
+        request
+    };
 };
