e78aa23b8536419f8a2a82c18157a314649f6474
Evgeniy <shiroleer@gmail.com>
2022-04-02 17:41:14 -0700

    options slice: add "activeType" to state, "getOptionsStatus" & "getOptionsActiveType" selectors

--- src/redux/slices/options.ts
+++ src/redux/slices/options.ts
@@ -1,13 +1,15 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import ky from "../../helpers/ky";
+import { Api } from '../../core/api';
 
-export const fetchOptions: any = createAsyncThunk('options/fetchOptions', async () => {
-    return await ky.get('/games').json();
+export const fetchOptions: any = createAsyncThunk('options/fetchOptions', async (arg, { getState }) => {
+    const state = getState() as any; 
+    return await Api.getOptionsList({ type: state.options.activeType, "from-time": "2022-01-01 00:00:00", "to-time": "2022-12-31 00:00:00", "order": "desc" })();
 });
 
 const baseState = {
     status: 'idle',
-    data: [] as any
+    data: [] as any,
+    activeType: 'all'
 };
 
 
@@ -15,26 +17,28 @@ const optionsSlice = createSlice({
     name: 'options',
     initialState: baseState,
     reducers: {
-        removeOption: (state, action) => {
-            state.data = state.data.filter(i => i.uuid != action.payload);
-        },
-        addOption: (state, action) => {
-            state.data.push(action.payload);
-        },
+        setActiveType: (state, action) => {
+            state.activeType = action.payload;
+        }
     },
     extraReducers: {
         [fetchOptions.pending]: (state, action) => {
             state.status = 'loading';
         },
+        [fetchOptions.rejected]: (state, action) => {
+            state.status = 'rejected';
+        },
         [fetchOptions.fulfilled]: (state, action) => {
             state.status = 'succeeded';
-            state.data = action.payload
+            state.data = action.payload.options_array
         },
     }
 })
 
 export default optionsSlice.reducer
 
-export const { addOption, removeOption } = optionsSlice.actions
+export const { setActiveType } = optionsSlice.actions
 
 export const getOptions = (state: any) => state.options.data;
+export const getOptionsStatus = (state: any) => state.options.status;
+export const getOptionsActiveType = (state: any) => state.options.activeType;
