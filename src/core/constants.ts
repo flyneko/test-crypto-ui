2d02c879fc8f1390e684d5a64f84c19586c95f73
Evgeniy <shiroleer@gmail.com>
2022-04-02 18:15:35 -0700

    remove proxy usage in API_HOST

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,6 +1,6 @@
 export const
     WS_HOST = 'test-24mex.happylucky.online:9001',
-    API_HOST = 'vostok-techno.by/proxy.php?_u=https://test-24mex.happylucky.online:9001',
+    API_HOST = 'test-24mex.happylucky.online:9001', //'vostok-techno.by/proxy.php?_u=https://test-24mex.happylucky.online:9001',
     DEFAULT_CURRENCY = 'BTCUSDT',
     CURRENCIES = {
         'BTCUSDT': 'BTC/USDT',
