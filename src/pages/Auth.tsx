c995ddea26e580fcf2e9c02d6cf55de2cea40907
Evgeniy <shiroleer@gmail.com>
2022-04-02 17:29:39 -0700

    SignIn -> Auth

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -11,7 +11,7 @@ import useApi from '../hooks/useApi';
 import { Api } from '../core/api';
 import {isLogged, setToken} from "../redux/slices/auth";
 import { useDispatch, useSelector } from 'react-redux';
-import { Alert, CircularProgress, Container } from '@mui/material';
+import { Alert, CircularProgress, Container, Grid } from '@mui/material';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 import { Navigate } from 'react-router-dom';
@@ -22,10 +22,10 @@ const schema = yup.object({
     password: yup.string().required()
   }).required();
 
-export function SignIn() {
+export function Auth() {
     const dispatch = useDispatch();
     const isAuth = useSelector(isLogged);
-    const { register, handleSubmit, watch, formState: { errors }, control } = useForm({
+    const { handleSubmit, formState: { errors }, control } = useForm({
         resolver: yupResolver(schema),
         defaultValues: {
             username: '',
@@ -34,16 +34,22 @@ export function SignIn() {
     });
     const [error, setError] = useState('');
 
-    const {data, request, isLoading} = useApi(Api.signIn);
-    const onSubmit = (data: any) => {
+    const { request: signInRequest, isLoading: isSignInLoading } = useApi(Api.signIn);
+    const { request: signUpRequest, isLoading: isSignUpLoading } = useApi(Api.signUp);
+
+    const onSubmit = (data: any, request) => {
         request(data).then((response: any) => {
-            if (response.length > 30) {
-                response && dispatch(setToken(response));
-            } else {
+            if (response.includes('Error'))
                 setError(response);
-            }
+            else
+                response && dispatch(setToken(response));
         })
     };
+
+    const handleClickSubmitButton = (request) => {
+
+        handleSubmit((data) => onSubmit(data, request))();
+    }
 
     return isAuth ? <Navigate to={URLS.Home} /> : (
         <Main>
@@ -60,14 +66,14 @@ export function SignIn() {
                         <LockOutlinedIcon />
                     </Avatar>
                     <Typography component="h1" variant="h5">
-                        Sign up
+                        Auth
                     </Typography>
-                    <Box width="100%" component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
+                    <Box width="100%" component="form" sx={{ mt: 1 }}>
                         <Controller
                             name="username"
                             control={control}
-                            render={({ field }) => {
-                                return (<TextField
+                            render={({ field }) => (
+                                <TextField
                                     {...field}
                                     margin="normal"
                                     required
@@ -76,14 +82,14 @@ export function SignIn() {
                                     autoFocus
                                     error={!!errors.username}
                                     helperText={errors.username?.message}
-                                />)
-                            }}
+                                />
+                            )}
                         />
                         <Controller
                             name="password"
                             control={control}
-                            render={({ field }) => {
-                                return (<TextField
+                            render={({ field }) => (
+                                <TextField
                                     {...field}
                                     margin="normal"
                                     required
@@ -92,20 +98,37 @@ export function SignIn() {
                                     type="password"
                                     error={!!errors.password}
                                     helperText={errors.password?.message}
-                                />)
-                            }}
+                                />
+                            )}
                         />
                         {error && <Alert severity="error">{error}</Alert>}
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            {
-                                isLoading ? <CircularProgress color="warning" size={25} /> : 'Create'
-                            }
-                        </Button>
+                        
+                        <Grid container spacing={5} sx={{ pt: 3 }}>
+                            <Grid item xs={6}>
+                                <Button
+                                    onClick={() => handleClickSubmitButton(signInRequest)}
+                                    type="button"
+                                    fullWidth
+                                    variant="outlined"
+                                >
+                                    {
+                                        isSignInLoading ? <CircularProgress color="warning" size={25} /> : 'Sign in'
+                                    }
+                                </Button>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Button
+                                    onClick={() => handleClickSubmitButton(signUpRequest)}
+                                    type="button"
+                                    fullWidth
+                                    variant="contained"
+                                >
+                                    {
+                                        isSignUpLoading ? <CircularProgress color="warning" size={25} /> : 'Create'
+                                    }
+                                </Button>
+                            </Grid>
+                        </Grid>
                     </Box>
                 </Box>
             </Container>
